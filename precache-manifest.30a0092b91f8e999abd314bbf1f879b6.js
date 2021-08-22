e3b2eab91dacb48e76c2a5e76e4da57213639b41
LiYu <2248868140@qq.com>
2021-08-22 23:35:14 +0800

    deploy

---
+++ precache-manifest.30a0092b91f8e999abd314bbf1f879b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fc5598ac3315cf4243",
+    "url": "/morney-website/css/app.cf94bcba.css"
+  },
+  {
+    "revision": "62bff2d18a17ff0d8158922162e8cfee",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "96fc5598ac3315cf4243",
+    "url": "/morney-website/js/app.68b7e990.js"
+  },
+  {
+    "revision": "528970439bba194a7054",
+    "url": "/morney-website/js/chunk-vendors.e49e99f3.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
